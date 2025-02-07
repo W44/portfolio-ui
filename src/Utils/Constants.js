8c54b653d1ebc0467e1ac7e64fcf0aa9025a56b6
Arslaan Asad <73607166+W44@users.noreply.github.com>
2025-02-08 00:48:49 +0200

    refactoring

---
+++ src/Utils/Constants.js
@@ -0,0 +1,49 @@
+export const projects = [
+    {
+      title: "Cryptofi - Company Project",
+      duration: "04/07/2022 – 01/02/2023",
+      company: "Strategic Systems International",
+      description: [
+        "Commercial application providing user access to cryptocurrencies.",
+        "**Backend:** Flask(Python)",
+        "Worked with RESTful APIs",
+        "Implemented infrastructure as code (AWS)",
+        "Built Lambda functions in Flask",
+      ],
+      link: "https://cu-2.com/fintech/cryptofi/",
+    },
+    {
+      title: "Intellifi - Company Project",
+      duration: "02/02/2023 – 07/06/2024",
+      company: "Strategic Systems International",
+      description: [
+        "Commercial application catering to workforce management.",
+        "**Backend (ASP.NET Core):** RESTful APIs, code optimization, database schema design.",
+        "**Frontend (Angular):** Page creation & backend integration.",
+      ],
+      link: "https://www.intellify.com/",
+    },
+    {
+      title: "Final Year Project",
+      duration: "14/08/2021 – 04/07/2022",
+      company: "Department of Computer Science, FAST - NUCES",
+      description: [
+        "**Frontend:** Flutter",
+        "**Backend:** Node.js",
+        "Implemented YOLOv5 object detection on a custom dataset",
+        "Dataset augmentation, parameter optimization, and ETL processing.",
+      ],
+      link: "#",
+    },
+    {
+      title: "Mobile Shopping Application Design",
+      duration: "00/00/0000 – 00/00/0000",
+      company: "Hobby Project",
+      description: [
+        "Prototype developed using Figma",
+        "Mobile application design for food shopping cart",
+        "Utilised and implemented UI/UX concepts and designs",
+      ],
+      link: "https://www.figma.com/proto/SZGHGqg1oyQQ8c1SdAgpDR/Food-Cart-Final?page-id=0%3A1&node-id=1-332&node-type=frame&viewport=1415%2C1132%2C1.16&t=GCn7aFJo3CHJlZ2q-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A332",
+    }
+  ];
