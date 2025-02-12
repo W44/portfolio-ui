1f818287284f8392908cb08063671e0e5f76f80d
Arslaan Asad <73607166+W44@users.noreply.github.com>
2025-02-12 03:03:09 +0200

    heading correction

--- src/Componants/Body/Body.js
+++ src/Componants/Body/Body.js
@@ -12,7 +12,7 @@ const MainContent = () => {
       <main className="mt-16 p-4 text-white">
         <Picture imageSrc={"/IMG_1313.jpg"}/>
         <h1 className="text-3xl font-bold mt-8 flex justify-center items-center">Arslaan Asad</h1>
-        <h2 className="text-xl mt-2 flex justify-center items-center ">{/*Software Engineer*/} <span className="text-gray-400"> (Fullstack Developer)</span></h2>
+        <h2 className="text-xl mt-2 flex justify-center items-center ">Software Engineer <span className="text-gray-400"> {/*(Fullstack Developer)*/}</span></h2>
     <Description/>
     <br/>
     <br/>
