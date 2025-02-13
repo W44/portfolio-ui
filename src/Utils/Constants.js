bc3670c66a5bc2545442823fcc983726b54ddb99
Arslaan Asad <73607166+W44@users.noreply.github.com>
2025-02-13 19:31:58 +0200

    projects information update

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,78 +1,76 @@
 export const projects = [
-    {
-      title: "Cryptofi - Company Project",
-      duration: "04/07/2022 – 01/02/2023",
-      company: "Strategic Systems International",
-      description: [
-        "Commercial application providing user access to cryptocurrencies.",
-        "**Backend:** Flask(Python)",
-        "Worked with RESTful APIs",
-        "Implemented infrastructure as code (AWS)",
-        "Built Lambda functions in Flask",
-      ],
-      link: "https://cu-2.com/fintech/cryptofi/",
-    },
-    {
-      title: "Intellifi - Company Project",
-      duration: "02/02/2023 – 07/06/2024",
-      company: "Strategic Systems International",
-      description: [
-        "Commercial application catering to workforce management.",
-        "**Backend (ASP.NET Core):** RESTful APIs, code optimization, database schema design.",
-        "**Frontend (Angular):** Page creation & backend integration.",
-      ],
-      link: "https://www.intellify.com/",
-    },
-    {
-      title: "Final Year Project",
-      duration: "14/08/2021 – 04/07/2022",
-      company: "Department of Computer Science, FAST - NUCES",
-      description: [
-        "**Frontend:** Flutter",
-        "**Backend:** Node.js",
-        "Implemented YOLOv5 object detection on a custom dataset",
-        "Dataset augmentation, parameter optimization, and ETL processing.",
-      ],
-      link: "#",
-    },
-    {
-      title: "Mobile Shopping Application Design",
-      duration: "00/00/0000 – 00/00/0000",
-      company: "Hobby Project",
-      description: [
-        "Prototype developed using Figma",
-        "Mobile application design for food shopping cart",
-        "Utilised and implemented UI/UX concepts and designs",
-      ],
-      link: "https://www.figma.com/proto/SZGHGqg1oyQQ8c1SdAgpDR/Food-Cart-Final?page-id=0%3A1&node-id=1-332&node-type=frame&viewport=1415%2C1132%2C1.16&t=GCn7aFJo3CHJlZ2q-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A332",
-    },
-    {
-      title: "Food Sharing Web Application",
-      duration: "00/00/0000 – 00/00/0000",
-      company: "Hobby Project",
-      description: [
-        "Frontend prototype developed using React",
-        "Food sharing application",
-        "Users publish food sharing requests to be fulfilled",
-        "Once fulfilled, the request can be deleted",
-        "Utilized and implemented UI/UX concepts and designs"
-      ],
-      link: "https://github.com/W44/React-FrontEnd-FO",
-    },
-    {
-      "title": "Flask Authentication Application",
-      "duration": "00/00/0000 – 00/00/0000",
-      "company": "Hobby Project",
-      "description": [
-        "Backend developed using Flask (Python)",
-        "Provides CRUD functionality",
-        "The main purpose was to explore and understand authentication flows",
-        "Custom authentication implementation",
-        "Implemented JWT-based authentication",
-        "SQLite DB implementation"
-      ],
-      "link": "https://github.com/W44/Flask_CRUD"
-    }
-
-
-  ];
+  {
+    title: "Cryptofi - Company Project",
+    duration: "04/07/2022 – 01/02/2023",
+    company: "Strategic Systems International",
+    description: [
+      "Commercial application providing user access to cryptocurrencies.",
+      "**Backend:** Flask (Python)",
+      "Worked with RESTful APIs.",
+      "Implemented infrastructure as code (AWS).",
+      "Built Lambda functions in Flask.",
+    ],
+    link: "https://cu-2.com/fintech/cryptofi/",
+  },
+  {
+    title: "Intellifi - Company Project",
+    duration: "02/02/2023 – 07/06/2024",
+    company: "Strategic Systems International",
+    description: [
+      "Commercial application catering to workforce management.",
+      "**Backend (ASP.NET Core):** RESTful APIs, code optimization, database schema design.",
+      "**Frontend (Angular):** Page creation & backend integration.",
+    ],
+    link: "https://www.intellify.com/",
+  },
+  {
+    title: "Final Year Project",
+    duration: "14/08/2021 – 04/07/2022",
+    company: "Department of Computer Science, FAST - NUCES",
+    description: [
+      "A computer vision-based application using YOLOv5 for object detection.",
+      "**Frontend:** Flutter",
+      "**Backend:** Node.js",
+      "Implemented YOLOv5 object detection on a custom dataset.",
+      "Performed dataset augmentation, parameter optimization, and ETL processing.",
+    ],
+    link: "#",
+  },
+  {
+    title: "Mobile Shopping Application Design",
+    duration: "00/00/0000 – 00/00/0000",
+    company: "Hobby Project",
+    description: [
+      "Prototype design for a mobile application focused on food shopping.",
+      "**Tool:** Figma",
+      "Utilized and implemented UI/UX concepts and best practices.",
+    ],
+    link: "https://www.figma.com/proto/SZGHGqg1oyQQ8c1SdAgpDR/Food-Cart-Final?page-id=0%3A1&node-id=1-332&node-type=frame&viewport=1415%2C1132%2C1.16&t=GCn7aFJo3CHJlZ2q-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A332",
+  },
+  {
+    title: "Food Sharing Web Application",
+    duration: "00/00/0000 – 00/00/0000",
+    company: "Hobby Project",
+    description: [
+      "A web application that facilitates food sharing among users.",
+      "**Frontend:** React",
+      "Developed a prototype for a food-sharing application.",
+      "Users can publish, fulfill, and delete food-sharing requests.",
+      "Applied modern UI/UX principles in the design.",
+    ],
+    link: "https://github.com/W44/React-FrontEnd-FO",
+  },
+  {
+    title: "Flask Authentication Application",
+    duration: "00/00/0000 – 00/00/0000",
+    company: "Hobby Project",
+    description: [
+      "An authentication application built to explore secure authentication flows.",
+      "**Backend:** Flask (Python)",
+      "**Database:** SQLite",
+      "Developed an application with full CRUD functionality.",
+      "Implemented custom authentication flows using JWT.",
+    ],
+    link: "https://github.com/W44/Flask_CRUD",
+  }
+];
