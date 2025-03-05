c6a36ef7bf6d076a190fb0144ecde41b0a0636e0
Arslaan Asad <73607166+W44@users.noreply.github.com>
2025-03-05 22:45:48 +0200

    updating Projects

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -24,41 +24,18 @@ export const projects = [
     link: "https://www.intellify.com/",
   },
   {
-    title: "Final Year Project",
-    duration: "14/08/2021 – 04/07/2022",
-    company: "Department of Computer Science, FAST - NUCES",
-    description: [
-      "A computer vision-based application using YOLOv5 for object detection.",
-      "**Frontend:** Flutter",
-      "**Backend:** Node.js",
-      "Implemented YOLOv5 object detection on a custom dataset.",
-      "Performed dataset augmentation, parameter optimization, and ETL processing.",
-    ],
-    link: "#",
-  },
-  {
-    title: "Mobile Shopping Application Design",
-    duration: "00/00/0000 – 00/00/0000",
-    company: "Hobby Project",
-    description: [
-      "Prototype design for a mobile application focused on food shopping.",
-      "**Tool:** Figma",
-      "Utilized and implemented UI/UX concepts and best practices.",
-    ],
-    link: "https://www.figma.com/proto/SZGHGqg1oyQQ8c1SdAgpDR/Food-Cart-Final?page-id=0%3A1&node-id=1-332&node-type=frame&viewport=1415%2C1132%2C1.16&t=GCn7aFJo3CHJlZ2q-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A332",
-  },
-  {
     title: "Food Sharing Web Application",
     duration: "00/00/0000 – 00/00/0000",
     company: "Hobby Project",
     description: [
       "A web application that facilitates food sharing among users.",
       "**Frontend:** React",
+      "**Backend:** Java Spring",
       "Developed a prototype for a food-sharing application.",
       "Users can publish, fulfill, and delete food-sharing requests.",
       "Applied modern UI/UX principles in the design.",
     ],
-    link: "https://github.com/W44/React-FrontEnd-FO",
+    link: "https://react-frontend-fo.pages.dev/login",
   },
   {
     title: "Flask Authentication Application",
@@ -72,5 +49,29 @@ export const projects = [
       "Implemented custom authentication flows using JWT.",
     ],
     link: "https://github.com/W44/Flask_CRUD",
+  },
+  {
+    title: "Mobile Shopping Application Design",
+    duration: "00/00/0000 – 00/00/0000",
+    company: "Hobby Project",
+    description: [
+      "Prototype design for a mobile application focused on food shopping.",
+      "**Tool:** Figma",
+      "Utilized and implemented UI/UX concepts and best practices.",
+    ],
+    link: "https://www.figma.com/proto/SZGHGqg1oyQQ8c1SdAgpDR/Food-Cart-Final?page-id=0%3A1&node-id=1-332&node-type=frame&viewport=1415%2C1132%2C1.16&t=GCn7aFJo3CHJlZ2q-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A332",
+  },
+  {
+    title: "Final Year Project",
+    duration: "14/08/2021 – 04/07/2022",
+    company: "Department of Computer Science, FAST - NUCES",
+    description: [
+      "A computer vision-based application using YOLOv5 for object detection.",
+      "**Frontend:** Flutter",
+      "**Backend:** Node.js",
+      "Implemented YOLOv5 object detection on a custom dataset.",
+      "Performed dataset augmentation, parameter optimization, and ETL processing.",
+    ],
+    link: "#",
   }
 ];
