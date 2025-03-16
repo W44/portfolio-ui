fa86f09684143919bf8b6ec6d92d32921bb826ab
Arslaan Asad <73607166+W44@users.noreply.github.com>
2025-03-16 14:20:00 +0200

    updating project details

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -6,12 +6,11 @@ export const projects = [
     company: "Hobby Project",
     description: [
       "A web application that facilitates food sharing among users.",
-      "**Frontend:** React",
-      "**Backend:** Java Spring",
       "Developed a prototype for a food-sharing application.",
       "Users can publish, fulfill, and delete food-sharing requests.",
       "Applied modern UI/UX principles in the design.",
     ],
+    skills: ["Java Spring", "React"],
     link: "https://react-frontend-fo.pages.dev/login",
   },
   {
@@ -20,11 +19,10 @@ export const projects = [
     company: "Hobby Project",
     description: [
       "An authentication application built to explore secure authentication flows.",
-      "**Backend:** Flask (Python)",
-      "**Database:** SQLite",
       "Developed an application with full CRUD functionality.",
       "Implemented custom authentication flows using JWT.",
     ],
+    skills: ["Flask(Python)", "SQLite"],
     link: "https://github.com/W44/Flask_CRUD",
   },
   {
@@ -33,9 +31,9 @@ export const projects = [
     company: "Hobby Project",
     description: [
       "Prototype design for a mobile application focused on food shopping.",
-      "**Tool:** Figma",
       "Utilized and implemented UI/UX concepts and best practices.",
     ],
+    skills: ["Figma", "UI/UX Design"],
     link: "https://www.figma.com/proto/SZGHGqg1oyQQ8c1SdAgpDR/Food-Cart-Final?page-id=0%3A1&node-id=1-332&node-type=frame&viewport=1415%2C1132%2C1.16&t=GCn7aFJo3CHJlZ2q-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A332",
   },
   {
@@ -44,11 +42,11 @@ export const projects = [
     company: "Strategic Systems International",
     description: [
       "Commercial application providing user access to cryptocurrencies.",
-      "**Backend:** Flask (Python)",
       "Worked with RESTful APIs.",
       "Implemented infrastructure as code (AWS).",
       "Built Lambda functions in Flask.",
     ],
+    skills: ["AWS", "Flask", "RESTful APIs", "Lambda"],
     link: "https://cu-2.com/fintech/cryptofi/",
   },
   {
@@ -60,6 +58,7 @@ export const projects = [
       "**Backend (ASP.NET Core):** RESTful APIs, code optimization, database schema design.",
       "**Frontend (Angular):** Page creation & backend integration.",
     ],
+    skills: ["ASP.NET Core", "Angular", "RESTful APIs", "Database Design", "Code Refactoring"],
     link: "https://www.intellify.com/",
   },
   {
@@ -68,11 +67,10 @@ export const projects = [
     company: "Department of Computer Science, FAST - NUCES",
     description: [
       "A computer vision-based application using YOLOv5 for object detection.",
-      "**Frontend:** Flutter",
-      "**Backend:** Node.js",
       "Implemented YOLOv5 object detection on a custom dataset.",
       "Performed dataset augmentation, parameter optimization, and ETL processing.",
     ],
+    skills: ["Flutter", "Node.js", "YOLOv5", "ETL", "Machine Learning"],
     link: "#",
   },
 ];
